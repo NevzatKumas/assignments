d1f573617a037af91ab92353556cd0af08ba12bf
Nevzat Kumas <kumasnevzat@gmail.com>
2021-10-10 18:04:19 -0400

    Add files via upload

--- test.c
+++ test.c
@@ -1,34 +1,23 @@
-#include <stdio.h>
+#include<stdio.h>
 #include <dirent.h>
-#include <sys/types.h>
-#include <string.h>
+#include<string.h>
+#include<sys/dir.h>
 #include<stdlib.h>
-#include<sys/stat.h>
-#include<time.h>
-#include<pwd.h>
-int main (int argc, char *argv[]){
-   
-    struct dirent **datapath;
-    struct stat filestat;
-    struct passwd *pw;
-    
-    int n;
-    int i=3;
-    n = scandir(".", &datapath,NULL,alphasort);
-    //stat(n,&filestat);
-    
-    if (n < 0)
+int main(void)
+{
+   struct dirent **namelist;
+   int n;
+   int i=2;
+   n = scandir(".", &namelist,NULL,alphasort);
+   if (n < 0)
       perror("scandir");
    else {
       while (i<n) {
-         stat(datapath[i]->d_name, &filestat);
-         printf("%s\n", datapath[i]->d_name);
-         printf("%s", ctime(&filestat.st_mtime));
-         
-         free(datapath[i]);
+         printf("%s\n", namelist[i]->d_name);
+         free(namelist[i]);
          ++i;
       }
-      free(datapath);
+      free(namelist);
    }
  return 0;
 }
